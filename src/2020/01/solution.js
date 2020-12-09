7dcde63b1cd786aad1d43d7fc1f2373a359301a0
Vincent Boiteau-Robert <vboiteau94@gmail.com>
2020-12-09 05:51:32 -0500

    Add 9th day solution

--- src/2020/01/solution.js
+++ src/2020/01/solution.js
@@ -1,11 +1,11 @@
-export function multiplyTwoNumberSummingToTarget(target, list) {
+export function twoSum(target, list) {
     const first = list.find((number) => list.includes(target - number));
     return first && first * (target - first);
 }
 
-export function multiplyThreeNumberSummingToTarget(target, list) {
+export function threeSum(target, list) {
     for (const number of list) {
-        const mult = multiplyTwoNumberSummingToTarget(target - number, list);
+        const mult = twoSum(target - number, list);
         if (mult) {
             return mult * number;
         }
--- src/2020/01/test.js
+++ src/2020/01/test.js
@@ -1,10 +1,10 @@
-import { multiplyThreeNumberSummingToTarget, multiplyTwoNumberSummingToTarget } from './solution';
+import { twoSum, threeSum } from './solution';
 import input from './input.json';
 
 test('Given a list of number when getInputSumTo2020 then the correct value should be returned.', () => {
-    expect(multiplyTwoNumberSummingToTarget(2020, input)).toBe(157059);
+    expect(twoSum(2020, input)).toBe(157059);
 });
 
 test('Given a list of number when getInputSumTo2020 then the correct value should be returned.', () => {
-    expect(multiplyThreeNumberSummingToTarget(2020, input)).toBe(165080960);
+    expect(threeSum(2020, input)).toBe(165080960);
 });
