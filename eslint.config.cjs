128997dd5ab138a15b58fa53a858c7185c06ef26
Vincent Boiteau-Robert <vboiteau94@gmail.com>
2025-12-13 14:33:00 -0500

    chore(eslint): move env/globals into languageOptions for flat config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -6,20 +6,20 @@ module.exports = [
       parserOptions: {
         project: ['./tsconfig.json'],
         sourceType: 'module'
+      },
+      ecmaVersion: 2022,
+      globals: {
+        test: 'readonly',
+        expect: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        vi: 'readonly'
       }
     },
     plugins: {
       '@typescript-eslint': require('@typescript-eslint/eslint-plugin')
-    },
-    env: { node: true, es2022: true },
-    globals: {
-      test: 'readonly',
-      expect: 'readonly',
-      describe: 'readonly',
-      it: 'readonly',
-      beforeEach: 'readonly',
-      afterEach: 'readonly',
-      vi: 'readonly'
     },
     settings: {},
     rules: {}
